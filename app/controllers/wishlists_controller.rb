76655ed694c3d705c128ba84ac3fd70b4c5915dd
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2021-03-27 21:35:21 +0900

    wishlist コントローラー編集

---
+++ app/controllers/wishlists_controller.rb
@@ -0,0 +1,58 @@
+class WishlistsController < ApplicationController
+  before_action :set_wishlist, only: [:edit, :show,]
+
+  def index
+    @wishlists = Wishlist.all
+  end
+
+  def new
+    @wishlist = Wishlist.new
+    @wishlists = Wishlist.all
+  end
+
+  def create
+    @wishlist = Wishlist.new(wishlist_params)
+    if @wishlist.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+    wishlist = Wishlist.find(params[:id])
+    wishlist.update(wishlist_params)
+    if wishlist.save
+      redirect_to wishlist_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    wishlist = Wishlist.find(params[:id])
+    wishlist.destroy
+    if wishlist.delete
+      redirect_to root_path(@wishlist)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def wishlist_params
+    params.require(:wishlist).permit(:name, :price, :image, :memo).merge(user_id: current_user.id)
+  end
+
+  def set_wishlist
+    @wishlist = Wishlist.find(params[:id])
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  
+  has_many :wishlists
 end
